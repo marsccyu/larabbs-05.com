1b3882004f7705b3af1b65e06d88f507a2a68d1b
Mars <marschen@omusic.com.tw>
2022-07-04 11:26:31 +0800

    第二章 基礎設置及樣式

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-start">
+            由 <a href="https://leo108.com" target="_blank">M</a> design
+        </p>
+
+        <p class="float-end"><a href="mailto:name@email.com">contact</a></p>
+    </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', '- index - ')
+
+@section('content')
+    <h1>index</h1>
+@stop
